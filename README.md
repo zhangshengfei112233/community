a6e1d1e89144a73537851e5e03c24fca9bdb833f
duxin_global <2812931505@qq.com>
2020-03-11 15:37:31 +0800

    Zhang community

--- README.md
+++ README.md
@@ -5,7 +5,23 @@
 [Spring Web](https://spring.io/guides/gs/serving-web-content/)
 [BootStrap文档](https://v3.bootcss.com/getting-started/)
 [Github Oauth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
-
+[Spring](https://docs.spring.io/spring-boot/docs/2.2.5.RELEASE/reference/html/spring-boot-features.html#boot-features-sql)
 ## 工具
 [Git](http://git-scm.com/download)
 [Visual Paradigm](http://www.visual-paradigm.com)
+
+##脚本
+```sql
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` varchar(100) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `token` char(100) DEFAULT NULL,
+  `gmt_create` bigint(20) DEFAULT NULL,
+  `gmt_modified` bigint(20) DEFAULT NULL,
+  `new_column` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
+
+
+```
--- src/main/java/zhang/community/community/controller/AuthorzeController.java
+++ src/main/java/zhang/community/community/controller/AuthorzeController.java
@@ -3,14 +3,17 @@ package zhang.community.community.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import zhang.community.community.dto.AccessTokenDTO;
 import zhang.community.community.dto.GithubUser;
+import zhang.community.community.mapper.UserMapper;
+import zhang.community.community.model.User;
 import zhang.community.community.provider.GithubProvider;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorzeController {
@@ -27,6 +30,9 @@ public class AuthorzeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -38,9 +44,18 @@ public class AuthorzeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user != null) {
-            request.getSession().setAttribute("user",user);
+
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(accessToken);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+
+            request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
             //登陆成功，写cookie和session
 
