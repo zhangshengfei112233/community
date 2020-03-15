377e7d57dcaab6f047d6dc1e0a695926337d3caf
duxin_global <2812931505@qq.com>
2020-03-15 22:28:46 +0800

    Zhang community

--- src/main/java/zhang/community/community/controller/CommentController.java
+++ src/main/java/zhang/community/community/controller/CommentController.java
@@ -2,10 +2,7 @@ package zhang.community.community.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import zhang.community.community.dto.CommentDTO;
 import zhang.community.community.mapper.CommentMapper;
 import zhang.community.community.model.Comment;
@@ -16,11 +13,13 @@ public class CommentController {
     @Autowired
     private CommentMapper commentMapper;
 
-    @RequestMapping(value = "/comment" ,method = RequestMethod.POST )
-    public Object post(@RequestBody CommentDTO commentDTO){
+    @ResponseBody
+    @RequestMapping(value = "/comment", method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO) {
         Comment comment = new Comment();
         comment.setParentId(commentDTO.getParentId());
         comment.setCommentator(1);
+        comment.setLikeCount(0L);
         comment.setGmtCreate(System.currentTimeMillis());
         comment.setGmtModified(System.currentTimeMillis());
         comment.setType(commentDTO.getType());
